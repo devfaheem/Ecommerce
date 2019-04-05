90e788c0e34a641f7b7d087239179d766af61efb
sharathkumarv <sharathkumarv183@gmail.com>
2019-04-05 17:54:46 +0530

    confirmation delete dailog box for brands,products and product category in admin side

--- resources/views/admin/productcategory/index.blade.php
+++ resources/views/admin/productcategory/index.blade.php
@@ -52,10 +52,8 @@
       <td>{{ $i++ }}</td>
        <td>{{ $value->name }}</td>
       <td><a href="{{ route('productcategory.edit',$value->id) }}" class="btn btn-info btn-square"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>&nbsp;&nbsp;&nbsp; 
-        {!! Form::open(['method' => 'DELETE','route' => ['productcategory.destroy', $value->id],'style'=>'display:inline']) !!}
-            <button type="submit" class="btn btn-danger btn-square"><i class="fa fa-trash" aria-hidden="true"></i>&nbsp;Delete</button>
-            {!! Form::close() !!}
-          </td>
+      <a href="javascript:;" data-toggle="modal" onclick="deleteData({{$value->id}})" data-target="#DeleteModal" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i> Delete</a> 
+          </td> 
     </tr>
     @endforeach 
   </tbody>
@@ -82,6 +80,46 @@
     </div>
   </main>
   <!--Main layout-->
+
+  <div id="DeleteModal" class="modal fade text-danger" role="dialog">
+   <div class="modal-dialog ">
+     <!-- Modal content-->
+     <form action="" id="deleteForm" method="post">
+         <div class="modal-content">
+             <div class="modal-header bg-danger">
+                 <button type="button" class="close" data-dismiss="modal">&times;</button>
+                 <h4 class="modal-title text-center">DELETE CONFIRMATION</h4>
+             </div>
+             <div class="modal-body">
+                 {{ csrf_field() }}
+                 {{ method_field('DELETE') }}
+                 <p class="text-center">Are You Sure Want To Delete ?</p>
+             </div>
+             <div class="modal-footer">
+                 <center>
+                     <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+                     <button type="submit" name="" class="btn btn-danger" data-dismiss="modal" onclick="formSubmit()">Yes, Delete</button>
+                 </center>
+             </div>
+         </div>
+     </form>
+   </div>
+  </div>
+
+   <script type="text/javascript">
+     function deleteData(id)
+     {
+         var id = id;
+         var url = '{{ route("productcategory.destroy", ":id") }}';
+         url = url.replace(':id', id);
+         $("#deleteForm").attr('action', url);
+     }
+
+     function formSubmit()
+     {
+         $("#deleteForm").submit();
+     }
+  </script>
 
   @endsection
 
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -99,9 +99,7 @@
          <td><a href='{{ asset("images/$value->image3") }}'><img src="{{ asset('images/'.$value->image3) }}" width="100px"></a></td>
         <td>{{ $value->status }}</td> 
       <td><a href="{{ route('product.edit',$value->id) }}" class="btn btn-info btn-square"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>&nbsp;&nbsp;&nbsp; 
-        {!! Form::open(['method' => 'DELETE','route' => ['product.destroy', $value->id],'style'=>'display:inline']) !!}
-            <button type="submit" class="btn btn-danger btn-square"><i class="fa fa-trash" aria-hidden="true"></i>&nbsp;Delete</button>
-            {!! Form::close() !!}
+            <a href="javascript:;" data-toggle="modal" onclick="deleteData({{$value->id}})" data-target="#DeleteModal" class="btn btn-danger btn-square"><i class="fa fa-trash"></i> Delete</a> 
           </td>
     </tr>
     @endforeach
@@ -145,6 +143,46 @@
     </div>
   </main>
   <!--Main layout-->
+
+  <div id="DeleteModal" class="modal fade text-danger" role="dialog">
+   <div class="modal-dialog ">
+     <!-- Modal content-->
+     <form action="" id="deleteForm" method="post">
+         <div class="modal-content">
+             <div class="modal-header bg-danger">
+                 <button type="button" class="close" data-dismiss="modal">&times;</button>
+                 <h4 class="modal-title text-center">DELETE CONFIRMATION</h4>
+             </div>
+             <div class="modal-body">
+                 {{ csrf_field() }}
+                 {{ method_field('DELETE') }}
+                 <p class="text-center">Are You Sure Want To Delete ?</p>
+             </div>
+             <div class="modal-footer">
+                 <center>
+                     <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+                     <button type="submit" name="" class="btn btn-danger" data-dismiss="modal" onclick="formSubmit()">Yes, Delete</button>
+                 </center>
+             </div>
+         </div>
+     </form>
+   </div>
+  </div>
+
+   <script type="text/javascript">
+     function deleteData(id)
+     {
+         var id = id;
+         var url = '{{ route("product.destroy", ":id") }}';
+         url = url.replace(':id', id);
+         $("#deleteForm").attr('action', url);
+     }
+
+     function formSubmit()
+     {
+         $("#deleteForm").submit();
+     }
+  </script>
 
 @endsection
 
